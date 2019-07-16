c27b72a77a6a5db49eb447d69a841ba032d1c286
Vallurupalli, Khetan <Khetan.Vallurupalli@wexinc.com>
2019-07-16 15:45:28 -0400

    final

--- db/postgresSqlUtil.js
+++ db/postgresSqlUtil.js
@@ -5,7 +5,6 @@ const { Pool, Client } = require("pg");
 //console.log(process.env.DATABASE_URL)
 const client = new Client({
   connectionString: "postgres://jcrljsigcywxnc:d70f83a5e4f944480eec913f4ffc567aa1bda97495c0b378ddd29de6140e496a@ec2-23-21-160-38.compute-1.amazonaws.com:5432/d159e6c8m0ir3u",
-  ssl: true
 });
 console.log(process.env.DATABASE_URL)
 client.connect();
